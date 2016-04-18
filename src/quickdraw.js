15da44fbb88dc60af02e086ec48e65aaf805614e
BillMills <mills.wj@gmail.com>
2016-04-18 12:25:35 -0700

    setters & mouse interactions

--- src/quickdraw.js
+++ src/quickdraw.js
@@ -29,6 +29,7 @@ function quickdraw(width, height){
 
         var i, j;
 
+        // clear the display canvas
         this.ctx.clearRect(0, 0, this.canvas.width, this.canvas.height);
 
         for(i=0; i<this.layers.length; i++){
@@ -37,16 +38,16 @@ function quickdraw(width, height){
 
             // update the layer as needed
             if(this.layers[i].needsUpdate){
-                // clear the existing canvas
+                // clear this layer's canvas
                 this.layers[i].ctx.clearRect(0, 0, this.layers[i].canvas.width, this.layers[i].canvas.height);
                 // redraw everything
                 for(j=0; j<this.layers[i].members.length; j++){
-                    this.layers[i].ctx.lineWidth = this.layers[i].members[j].lineWidth;
+                    this.layers[i].ctx.lineWidth = this.layers[i].members[j]._lineWidth;
                     this.layers[i].ctx.fillStyle = this.layers[i].members[j]._fillStyle;
-                    this.layers[i].ctx.strokeStyle = this.layers[i].members[j].strokeStyle;
+                    this.layers[i].ctx.strokeStyle = this.layers[i].members[j]._strokeStyle;
 
-                    this.layers[i].ctx.fill(this.layers[i].members[j].path);
-                    this.layers[i].ctx.stroke(this.layers[i].members[j].path);
+                    this.layers[i].ctx.fill(this.layers[i].members[j]._path);
+                    this.layers[i].ctx.stroke(this.layers[i].members[j]._path);
                 }
                 this.layers[i].needsUpdate = false;
             }
@@ -54,9 +55,7 @@ function quickdraw(width, height){
             // print the layer on the display canvas
             this.ctx.drawImage(this.layers[i].canvas, 0, 0);
         }
-
         this.updateTargets();
-        
     },
 
     this.updateTargets = function(){
@@ -77,41 +76,122 @@ function quickdraw(width, height){
         }  
     }
 
-    this.clickInteraction = function(evt){
+    this.mouseInteraction = function(evt){
         //handle a click interaction
         var i,
             bounds = this.canvas.getBoundingClientRect();
 
         for(i=0; i<this.touchTargets.length; i++){
-            if(this.ctx.isPointInPath(this.touchTargets[i].path, evt.clientX - bounds.left, evt.clientY - bounds.top)){
-                console.log(this.touchTargets[i]._fillStyle)
+            // are we touching shape i?
+            if(this.ctx.isPointInPath(this.touchTargets[i]._path, evt.clientX - bounds.left, evt.clientY - bounds.top)){
+                this.currentTouch = this.touchTargets[i];
+
+                if(evt.type == 'click')
+                    this.touchTargets[i]._click(evt.clientX - bounds.left, evt.clientY - bounds.top, evt);
+                else if(evt.type == 'mousemove'){
+                    if(!this.previousTouch){
+                        //move from empty space to shape
+                        this.currentTouch._mouseover(evt.clientX - bounds.left, evt.clientY - bounds.top, evt);
+                    } else if(this.previousTouch.id !== this.currentTouch.id){
+                        //left one shape and entered another
+                        this.previousTouch._mouseout(evt.clientX - bounds.left, evt.clientY - bounds.top, evt);
+                        this.currentTouch._mouseover(evt.clientX - bounds.left, evt.clientY - bounds.top, evt);
+                    } else { 
+                        //moving around in the same shape
+                        this.touchTargets[i]._mousemove(evt.clientX - bounds.left, evt.clientY - bounds.top, evt);
+                    }
+                }
+                this.previousTouch = this.currentTouch;
+                // bail out immediately once something is successfully touched
                 return 0
-            }
+            } 
+        }
+
+        if(this.previousTouch){
+            // move from shape to empty space
+            this.previousTouch._mouseout(evt.clientX - bounds.left, evt.clientY - bounds.top, evt);
+            this.previousTouch = null;
+            this.currentTouch = null;
         }
     }
-    this.canvas.onclick = this.clickInteraction.bind(this);
+    // bind listeners
+    this.canvas.onclick = this.mouseInteraction.bind(this);
+    this.canvas.onmousemove = this.mouseInteraction.bind(this);
+    this.canvas.onmouseout = function(){
+        this.currentTouch = null;
+        this.previousTouch = null;
+    }.bind(this)
 
 }
 
-function qdshape(path){
+function qdshape(path, id){
     // constructor for a generic shape; path == Path2D object that defines the shape
 
-    this.path = path;
-    this.lineWidth = 1;
-    this.strokeStyle = '#000000';
+    var propertySetter, interactionSetter;
+
+    // default parameters
+    this.id = id;
+    this._path = path;
+    this._lineWidth = 1;
+    this._strokeStyle = '#000000';
     this._fillStyle = '#000000';
     this.touchable = true;
-    this.z = 1;
+    this._z = 1;
     this.parentLayer = null;
 
+    // dummy interaction callbacks
+    this._click = function(){return 0};
+    this._mouseover = function(){return 0};
+    this._mousemove = function(){return 0};
+    this._mouseout = function(){return 0};
+
     // setters flag layers for redraw
-    Object.defineProperty(this, 'fillStyle', { 
-        set: function(fs){ 
-            this._fillStyle = fs;
-            if(this.parentLayer)
-                this.parentLayer.needsUpdate = true; 
-        } 
-    })
+    propertySetter = function(variableName, setValue){
+        this[variableName] = setValue;
+        if(this.parentLayer)
+            this.parentLayer.needsUpdate = true;
+    };
+
+    Object.defineProperty(this, 'path', {
+        set: propertySetter.bind(this, '_path')
+    });
+
+    Object.defineProperty(this, 'lineWidth', {
+        set: propertySetter.bind(this, '_lineWidth')
+    });
+
+    Object.defineProperty(this, 'strokeStyle', {
+        set: propertySetter.bind(this, '_strokeStyle')
+    });
+
+    Object.defineProperty(this, 'fillStyle', {
+        set: propertySetter.bind(this, '_fillStyle')
+    });
+
+    Object.defineProperty(this, 'z', {
+        set: propertySetter.bind(this, '_z')
+    });
+
+    // mouse interaction setters
+    interactionSetter = function(interactionName, callback){
+        this[interactionName] = callback.bind(this);
+    }
+
+    Object.defineProperty(this, 'click', {
+        set: interactionSetter.bind(this, '_click')
+    });
+
+    Object.defineProperty(this, 'mouseover', {
+        set: interactionSetter.bind(this, '_mouseover')
+    });
+
+    Object.defineProperty(this, 'mousemove', {
+        set: interactionSetter.bind(this, '_mousemove')
+    });
+
+    Object.defineProperty(this, 'mouseout', {
+        set: interactionSetter.bind(this, '_mouseout')
+    });
 }
 
 function qdlayer(name){
@@ -133,7 +213,7 @@ function qdlayer(name){
 
         // enforce z ordering
         this.members.sort(function(a, b) {
-            return a.z - b.z;
+            return a._z - b._z;
         });
     }
 }
